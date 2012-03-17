4d7863ffdcb2865334836eea8b5ebca798cb408f
Peng <neildong@gmail.com>
2012-03-18 03:49:42 +0800

    增加起卦方式：报数起卦和时间起卦

---
+++ PlayChanges/QiGua.h
@@ -0,0 +1,13 @@
+//
+//  QiGua.h
+//  PlayChanges
+//
+//  Created by 董鹏 on 12-3-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QiGua : UIViewController
+
+@end
