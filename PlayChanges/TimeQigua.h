b8582133c215ca20e8058c4e0310f9f85c120ae6
Peng <neildong@gmail.com>
2012-03-18 04:53:06 +0800

    添加报数起卦和时间起卦

---
+++ PlayChanges/TimeQigua.h
@@ -0,0 +1,13 @@
+//
+//  TimeQigua.h
+//  PlayChanges
+//
+//  Created by 董鹏 on 12-3-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimeQigua : UIViewController
+
+@end
---
+++ PlayChanges/TimeQigua.m
@@ -0,0 +1,44 @@
+//
+//  TimeQigua.m
+//  PlayChanges
+//
+//  Created by 董鹏 on 12-3-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "TimeQigua.h"
+
+@interface TimeQigua ()
+
+@end
+
+@implementation TimeQigua
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
